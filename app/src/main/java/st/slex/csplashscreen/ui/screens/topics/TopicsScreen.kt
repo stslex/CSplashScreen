01dd2a168fda2cc32c8f7836517b774d532ebc9a
stslex <ilya977.077@gmail.com>
2021-10-12 02:04:51 +0300

    add some style in Topics Screen

--- app/src/main/java/st/slex/csplashscreen/ui/screens/topics/TopicsScreen.kt
+++ app/src/main/java/st/slex/csplashscreen/ui/screens/topics/TopicsScreen.kt
@@ -1,12 +1,16 @@
 package st.slex.csplashscreen.ui.screens.topics
 
 import android.annotation.SuppressLint
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.graphicsLayer
@@ -25,6 +29,8 @@ import st.slex.csplashscreen.ui.MainActivity
 import st.slex.csplashscreen.ui.theme.Typography
 
 
+@ExperimentalAnimationApi
+@SuppressLint("RememberReturnType")
 @ExperimentalCoroutinesApi
 @ExperimentalCoilApi
 @ExperimentalPagerApi
@@ -37,32 +43,52 @@ fun TopicsScreen(
 ) {
     val lazyPagingItems = viewModel.topics.collectAsLazyPagingItems()
 
+    val paddingPagerState = remember { mutableStateOf(PaddingValues(start = 70.dp, end = 32.dp)) }
+    val paddingColumn = remember { mutableStateOf(16.dp) }
+    val imageModifier = remember { mutableStateOf(Modifier.clipToBounds()) }
+    val textModifierState = remember { mutableStateOf(Modifier.padding()) }
     HorizontalPager(
         count = lazyPagingItems.itemCount,
         state = pagerState,
-        contentPadding = PaddingValues(start = 70.dp, end = 32.dp)
+        contentPadding = paddingPagerState.value
     ) { page ->
 
         Column(
             modifier = Modifier
-                .padding(end = 16.dp)
+                .padding(end = paddingColumn.value)
                 .animationUtilPager(
                     scope = this@HorizontalPager,
                     page = page
                 )
         ) {
             Text(
+                modifier = textModifierState.value,
                 text = lazyPagingItems[page]?.title.toString(),
                 style = Typography.h5,
                 maxLines = 1
             )
-            Surface(modifier = Modifier.padding(top = 32.dp)) {
+            Surface(
+                modifier = Modifier
+                    .padding(top = 32.dp)
+                    .clickable {
+                        if (paddingPagerState.value == PaddingValues(0.dp)) {
+                            paddingPagerState.value = PaddingValues(start = 70.dp, end = 32.dp)
+                            paddingColumn.value = 16.dp
+                            imageModifier.value = Modifier.clipToBounds()
+                            textModifierState.value = Modifier.padding(0.dp)
+                        } else {
+                            paddingPagerState.value = PaddingValues(0.dp)
+                            paddingColumn.value = 0.dp
+                            imageModifier.value = Modifier.fillMaxSize()
+                            textModifierState.value = Modifier.padding(start = 16.dp)
+                        }
+                    }
+            ) {
                 Image(
-                    modifier = Modifier
-                        .clipToBounds(),
+                    modifier = imageModifier.value,
                     painter = rememberImagePainter(data = lazyPagingItems[page]?.cover_photo?.urls?.regular.toString()) {
-                        this.crossfade(500)
-                        this.transformations(RoundedCornersTransformation())
+                        crossfade(500)
+                        transformations(RoundedCornersTransformation())
                     },
                     contentDescription = ""
                 )
