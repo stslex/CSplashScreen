45c1c08d1b00bd797bf1e78cc4ecf21c59bf8f49
stslex <ilya977.077@gmail.com>
2023-07-05 02:02:46 +0300

    fix wallpaper and loader

--- feature-photo-detail/src/main/java/st/slex/feature_photo_detail/di/ModuleFeaturePhoto.kt
+++ feature-photo-detail/src/main/java/st/slex/feature_photo_detail/di/ModuleFeaturePhoto.kt
@@ -1,18 +1,18 @@
 package st.slex.feature_photo_detail.di
 
+import com.stslex.csplashscreen.core.core.AppModule
+import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
-import com.stslex.csplashscreen.core.core.AppModule
 import st.slex.feature_photo_detail.data.PhotoRepository
 import st.slex.feature_photo_detail.data.PhotoRepositoryImpl
 import st.slex.feature_photo_detail.ui.DetailPhotoViewModel
 import st.slex.feature_photo_detail.ui.utils.DownloadImageUseCase
 import st.slex.feature_photo_detail.ui.utils.DownloadImageUseCaseImpl
-import st.slex.feature_photo_detail.ui.utils.WallpaperCustomTarget
 import st.slex.feature_photo_detail.ui.utils.WallpaperSetUseCase
 import st.slex.feature_photo_detail.ui.utils.WallpaperSetUseCaseImpl
 
@@ -23,10 +23,7 @@ class ModuleFeaturePhoto : AppModule {
         viewModelOf(::DetailPhotoViewModel)
         factoryOf(::DownloadImageUseCaseImpl) { bind<DownloadImageUseCase>() }
         factory<WallpaperSetUseCase> {
-            WallpaperSetUseCaseImpl(
-                context = get(),
-                target = WallpaperCustomTarget(get())
-            )
+            WallpaperSetUseCaseImpl(context = androidContext())
         }
     }
 }
